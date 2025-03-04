033bf59c20000b7f6f2be88630d58e6a729f684e
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-03-04 09:23:38 +0530

    thread

--- thread_initialize.c
+++ thread_initialize.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <omp.h>
 
 int main()
 {
@@ -8,14 +9,23 @@ int main()
     const int N = 100;
     int A[N], B[N];
     long long sum = 0;
+    int num_threads = 4; // Set the desired number of threads
 
+    // Set the number of threads
+    omp_set_num_threads(num_threads);
+    printf("Using %d threads\n", num_threads);
+
+    // Initialize arrays
     for (int i = 0; i < N; i++)
     {
         A[i] = i;
         B[i] = i;
     }
+
     start = clock();
 
+// Parallel sum computation using OpenMP
+#pragma omp parallel for reduction(+ : sum)
     for (int i = 0; i < N; i++)
     {
         sum += (A[i] + B[i]);
@@ -25,7 +35,7 @@ int main()
 
     printf("Total sum: %lld\n", sum);
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Time taken: %f\n", cpu_time_used);
+    printf("Time taken: %f seconds\n", cpu_time_used);
 
     return 0;
 }
